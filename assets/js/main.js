eb774bbd6b365a2d61ce81d2158722417940969d
klee <lee.kev@husky.neu.edu>
2021-09-27 03:20:40 -0500

    added wallet connect support, mobile mint, and better error handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@
 const Sockz = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"who","type":"address"},{"indexed":false,"internalType":"uint256","name":"sockId","type":"uint256"}],"name":"Claim","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"baseURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"sockId","type":"uint256"}],"name":"claim","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"claimIsActive","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"deposit","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"flipClaimState","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"flipMintState","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"mintIsActive","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"toadId","type":"uint256"}],"name":"mintWithToad","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256[]","name":"sockIds","type":"uint256[]"}],"name":"multiClaimWithSockz","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256[]","name":"toadIds","type":"uint256[]"}],"name":"multiMintWithToad","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"baseURI","type":"string"}],"name":"setBaseURI","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"toadAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 const contractAddress = "0x537B9AF55daDcaD9D22309e5b8CE35CFFD8c1925"
 var errorMessageWrap = document.getElementById('errorMessage1');
+const WalletConnectProvider = window.WalletConnectProvider.default;
 // Is mobile?
 function ismobile() {
   let isMobile = window.matchMedia("only screen and (max-width: 740px)").matches;
@@ -89,7 +90,7 @@ ismobile();
 
   async function connectWalletConnect(){
     const webProvider = new WalletConnectProvider({
-      infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+      infuraId: "fb89a41620354beea8a39f509c52da63",
     });
     await webProvider.enable();
     provider = new ethers.providers.Web3Provider(webProvider);
@@ -123,20 +124,21 @@ ismobile();
   function closeError(){
     errorMessageWrap.style.display = "none"
   }
+  document.getElementsByClassName("errorBack")[0].addEventListener('click', closeError);
+
   function showError(){
     console.log("error thing", errorMessageWrap);
 
     errorMessageWrap.style.display = "block"
     errorMessageWrap.style.marginLeft = "0px"
 
-    /* setTimeout(function () {
-      errorMessageWrap.style.marginLeft = "-490px"
+    setTimeout(function () {
 
       setTimeout(function () {
         errorMessageWrap.style.display = "none"
-      }, 1000);
+      }, 3000);
 
-    }, 3000); */
+    }, 6000);
 
   }
 
@@ -294,20 +296,34 @@ ismobile();
 
     if (isConnected) {
       if (incrementValue.innerText > 1) {
-        var lot = prompt("Please enter the list of toadIds you would like to mint from: ", "1,2,3,...");
-        var toadIds = lot.split(',').map(Number);
-        console.log(toadIds);
-        const contract = new ethers.Contract(contractAddress, Sockz, signer);
-        const transaction = await contract.multiMintWithToad(toadIds);
-        await transaction.wait();
+        try {
+          var lot = prompt("Please enter the list of toadIds you would like to mint from: ", "1,2,3,...");
+          var toadIds = lot.split(',').map(Number);
+          console.log(toadIds);
+          const contract = new ethers.Contract(contractAddress, Sockz, signer);
+          const transaction = await contract.multiMintWithToad(toadIds);
+          await transaction.wait();
+        }
+        catch(err) {
+          document.getElementById("mintError").innerText = err["message"];
+          showError();
+          return;
+        }
       }
       else if (incrementValue.innerText == 1) {
-        var toadId = prompt("Please enter the toadId you would like to mint from: ", "toadId");
-        var toadInt = Number(toadId);
-        console.log(toadInt);
-        const contract = new ethers.Contract(contractAddress, Sockz, signer);
-        const transaction = await contract.mintWithToad(toadInt);
-        await transaction.wait();
+        try {
+          var toadId = prompt("Please enter the ID of the toad you would like to mint from: ", "toadID");
+          var toadInt = Number(toadId);
+          console.log(toadInt);
+          const contract = new ethers.Contract(contractAddress, Sockz, signer);
+          const transaction = await contract.mintWithToad(toadInt);
+          await transaction.wait();
+        }
+        catch(err) {
+          document.getElementById("mintError").innerText = err["message"];
+          showError();
+          return;
+        }
       }
       else {
         window.alert("please choose how many sockz you would like to mint");
